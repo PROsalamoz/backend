2ab0f50d819a50ab0bd58d3da8bc32ef313c74c4
mahmoud_salama <salamozm346@gmail.com>
2021-04-13 09:20:21 +0200

    add Category model

--- shops/views.py
+++ shops/views.py
@@ -1,7 +1,9 @@
-from .models import Shop
+from rest_framework import viewsets
+
+from .models import Shop, Category
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
-from .serializers import ShopSerializers
+from .serializers import ShopSerializers, CategorySerializers
 
 
 @api_view(['GET'])
@@ -9,3 +11,8 @@ def index(request):
     all_shops = Shop.objects.all()
     data = ShopSerializers(all_shops, many=True).data
     return Response([data])
+
+
+class Category(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializers
