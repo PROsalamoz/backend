8f72621381eaeb029d62c1af5c4a4081d6df9c81
alshimaa gamal <alshimaa.soger@gmail.com>
2021-04-27 08:42:00 -0700

    edit shop model

--- shops/models.py
+++ shops/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.utils.text import slugify
 
 
+
+def arabic_slugify(str):
+    str = str.replace(" ", "-")
+    str = str.replace(",", "-")
+    str = str.replace("(", "-")
+    str = str.replace(")", "")
+    str = str.replace("؟", "")
+    return str
+
+
 class Shop(models.Model):
+    ShopId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     slug = models.SlugField(blank=True, null=True)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
-    img = models.ImageField(upload_to='images')
+    img = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='shops_in_category')
 
     def __str__(self):
         return self.name
@@ -18,6 +30,8 @@ class Shop(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
+            if not self.slug:
+                self.slug = arabic_slugify(self.name)
         super(Shop, self).save(*args, **kwargs)
 
 
